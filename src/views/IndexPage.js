52d71ed05fa0e6c412bcba83ba77447e15523027
DPF <DINGDINGDINGSOLA@163.com>
2019-06-10 21:24:18 +0800

    合并完成

--- src/views/IndexPage.js
+++ src/views/IndexPage.js
@@ -3,13 +3,12 @@ import { connect } from 'dva';
 import styles from './IndexPage.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import {BrowserRouter,Link} from 'dva/router'
-import RouterConfig from '../router'
+// import RouterConfig from '../router'
 import Main from '../components/Main/index'
 import './IndexPage.css'
 
 function IndexPage() {
   return (
-    <BrowserRouter>
         <div className={styles.index}>
       <div className={styles.login}>
       <Form className="login-form" className={styles.login_form}>
@@ -30,16 +29,17 @@ function IndexPage() {
           <Checkbox>记住密码</Checkbox>
           <a className="login-form-forgot" href="">忘记密码</a><br/>
         </Form.Item>
-        <Link to="/main" component={Main}>
-        <Button type="primary" htmlType="submit" 
+        {/* <Link to="/main" component={Main}>
+       
+        </Link> */}
+         <Button type="primary" htmlType="submit" 
         className="login-form-button" className={styles.login_form_button}
         >登录</Button>
-        </Link>
       </Form>
       </div>
-        
+      <Main />
     </div>
-    </BrowserRouter>
+    
   );
 }
 
