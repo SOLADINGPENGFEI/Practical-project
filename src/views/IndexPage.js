eb976757d564b5f88228ddc26efc19362807ed52
DPF <DINGDINGDINGSOLA@163.com>
2019-06-10 17:31:18 +0800

    试卷管理页面完成90%

--- src/views/IndexPage.js
+++ src/views/IndexPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
+import Main from '../components/Main/index'
+import './IndexPage.css'
 
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
+    <div className="wrap" style={{width:"100%",height:"100%"}}>
+       <Main />
     </div>
   );
 }
